e1678918108a98a79084f6c5c1c1d301a100badb
Amlesh Sivanantham (zamlz) <zamlz@pm.me>
2021-11-30 20:43:10 -0800

    Added a really minimal interface

--- src/pyfinder/shell.py
+++ src/pyfinder/shell.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# TODO: Actually implement this
 class CommandCompletion(object):
     pass
 
@@ -8,8 +9,20 @@ class PathfinderShell(object):
 
     def run(self):
         while True:
-            x = raw_input("PF> ")
-            print(x)
+
+            user_input = input("PathFinder> ").split(' ')
+
+            # its kinda guaranteed to have atleast one item
+            user_cmd = user_input[0]
+            user_args = [] if len(user_input) == 1 else user_input[1:]
+
+            if user_cmd == '':
+                continue
+            elif user_cmd == 'quit' or user_cmd == 'exit':
+                exit(0)
+            else:
+                print(f"command: {user_cmd}")
+                print(f"command: {user_args}")
 
 
 def start_shell():
